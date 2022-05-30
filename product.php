3ffc48260e6c8f7b6145ede98c243e67c6a61554
Shashhhka <shabanovasasha@yandex.ru>
2022-05-30 07:12:51 +0500

    Стартовый проект html академии

--- product.php
+++ product.php
@@ -7,7 +7,7 @@ $id = $_GET['product_id'];
 $title = get_product_title($id);
 $img_url = get_img_url($id);
 if ($is_violet_tuesday) {
-  require('components/product_violet.php');
+    require('components/product_violet.php');
 }
 
 require('components/product_info.php');
